049a1886d0e8311ae515c398499fe31c3dec0857
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 19:12:33 -0300

    Lets test pushing the image to ECR using travis

--- ci/scripts/jobs/image_push.sh
+++ ci/scripts/jobs/image_push.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 echo "STARTING: ${0}"
+apk add python3
+pip3 install awscli
+docker build --compress -t $ECR_REPO:$TRAVIS_COMMIT
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker push $ECR_REPO:$TRAVIS_COMMIT
+docker tag $ECR_REPO:$TRAVIS_COMMIT:latest
+docker push $ECR_REPO:latest
