512a69ec15ef671aaf1887c6a6fbd83b82e21684
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 14:05:44 -0300

    getting the terraform installation on sudo

--- ci/sh/init/formation.sh
+++ ci/sh/init/formation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 wget https://releases.hashicorp.com/terraform/"${MXMD_FORMATION_VERSION}"/terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
 unzip terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
-mv terraform /usr/local/bin/
+sudo mv terraform /usr/local/bin/
 rm terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
