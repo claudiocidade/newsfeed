de14a157ffe14f57b2c60875318771136edff180
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 14:01:43 -0300

    Lets test remotely

--- ci/sh/init/formation.sh
+++ ci/sh/init/formation.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "STARTING: ${0}"
-wget https://releases.hashicorp.com/terraform/${MXMD_FORMATION_VERSION}/terraform_${MXMD_FORMATION_VERSION}_linux_amd64.zip
-unzip terraform_${MXMD_FORMATION_VERSION}_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-rm terraform_${MXMD_FORMATION_VERSION}_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/"${MXMD_FORMATION_VERSION}"/terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
+unzip terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
+mv terraform /usr/local/bin/
+rm terraform_"${MXMD_FORMATION_VERSION}"_linux_amd64.zip
