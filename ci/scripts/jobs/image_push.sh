4f5cf4653a0e94b1fc44820b0fa11e26a30514ff
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 19:48:22 -0300

    fixing the docker tag command

--- ci/scripts/jobs/image_push.sh
+++ ci/scripts/jobs/image_push.sh
@@ -5,5 +5,5 @@ pip3 install awscli
 docker build --compress -t $ECR_REPO:$TRAVIS_COMMIT
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker push $ECR_REPO:$TRAVIS_COMMIT
-docker tag $ECR_REPO:$TRAVIS_COMMIT:latest
+docker tag $ECR_REPO:$TRAVIS_COMMIT $ECR_REPO:latest
 docker push $ECR_REPO:latest
