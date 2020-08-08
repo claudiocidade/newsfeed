c5a3b0783e641144e3ca84bc353465f631b7f000
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 20:13:49 -0300

    Fixing the image_push setup

--- ci/scripts/jobs/image_push.sh
+++ ci/scripts/jobs/image_push.sh
@@ -3,7 +3,7 @@ echo "STARTING: ${0}"
 sudo apt-get update
 sudo apt-get install -y python3
 sudo apt-get install -y awscli
-docker build --compress -t $ECR_REPO:$TRAVIS_COMMIT
+docker build --compress -t $ECR_REPO:$TRAVIS_COMMIT -f "${TRAVIS_BUILD_DIR}/client/Dockerfile.dev" .
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker push $ECR_REPO:$TRAVIS_COMMIT
 docker tag $ECR_REPO:$TRAVIS_COMMIT $ECR_REPO:latest
