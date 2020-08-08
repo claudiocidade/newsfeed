9c09a0730b0855576742e5b5328dcef64b910c71
Claudio Cidade <claudiocidade@gmail.com>
2020-08-08 20:03:21 -0300

    Fixing the image_push setup

--- ci/scripts/jobs/image_push.sh
+++ ci/scripts/jobs/image_push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "STARTING: ${0}"
-sudo apt-get install python3 python3-pip
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
 sudo pip3 install awscli
 docker build --compress -t $ECR_REPO:$TRAVIS_COMMIT
 $(aws ecr get-login --no-include-email --region us-east-1)
