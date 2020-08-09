637207ad3dddc7ca04c242bb64975d66b7234871
Claudio Cidade <claudiocidade@gmail.com>
2020-08-09 12:43:21 -0300

    Applied some more clean up

--- ci/scripts/init/variables.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "STARTING ${0}"
-export $MXMD_ECR_REPO_UI=$MXMD
---
+++ ci/tools/terraform/_vpc-network.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "mxmd-vpc" {
+  # Only 16 addresses (14 hosts) available. 
+  # More than necessary for a demo.
+  cidr_block           = "10.1.0.0/28"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-vpc")
+  )
+}
+
+resource "aws_internet_gateway" "mxmd-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-igw")
+  )
+}
