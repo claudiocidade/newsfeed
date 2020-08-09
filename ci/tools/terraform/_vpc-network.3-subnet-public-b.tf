12419ddfc014eccf007dc463148bf22b43498161
Claudio Cidade <claudiocidade@gmail.com>
2020-08-09 14:10:11 -0300

    Formation is validated

--- ci/tools/terraform/_vpc-network.3-subnet-public-b.tf
+++ ci/tools/terraform/_vpc-network.3-subnet-public-b.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_b" {
   cidr_block              = "10.1.2.0/29"
-  map_public_ip_on_branch = true
-  vpc_id                  = aws_vpc.maxmd_vpc.id
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.mxmd_vpc.id
   availability_zone       = "${data.aws_region.current.name}a"
 
   tags = merge(
@@ -27,7 +27,7 @@ resource "aws_route_table_association" "public_b" {
 resource "aws_route" "public_internet_access_b" {
   route_table_id         = aws_route_table.public_b.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.public_b.id
+  gateway_id             = aws_internet_gateway.mxmd_igw.id
 }
 
 resource "aws_eip" "public_b" {
---
+++ ci/tools/terraform/_vpc-network.4-subnet-private-a.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "private_a" {
+  cidr_block              = "10.1.10.0/29"
+  vpc_id                  = aws_vpc.mxmd_vpc.id
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.mxmd_vpc.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
