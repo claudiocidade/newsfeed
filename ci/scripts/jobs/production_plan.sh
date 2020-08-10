106bfe959ae34365a5c9f52fed5261448d5d949a
Claudio Cidade <claudiocidade@gmail.com>
2020-08-10 11:24:24 -0300

    Fixing the paths to scripts since there were minor issues after clean up and including the ELB policy

--- ci/scripts/jobs/production_plan.sh
+++ ci/scripts/jobs/production_plan.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "STARTING: ${0}"
-bash ./ci/utils/terraform_initenv.sh
+bash ./ci/scripts/utils/terraform_initenv.sh
 bash ./ci/scripts/utils/terraform_exec.sh production plan
