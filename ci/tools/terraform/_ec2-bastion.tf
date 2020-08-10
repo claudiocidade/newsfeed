84ceb8151e01c85d6de15b29387f3a0a33fc4b06
Claudio Cidade <claudiocidade@gmail.com>
2020-08-10 01:09:40 -0300

    Finally finished setting up the bastion and how the terraform instance administers roles/policies and user data scripts

--- ci/tools/terraform/_ec2-bastion.tf
+++ ci/tools/terraform/_ec2-bastion.tf
@@ -7,19 +7,35 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_iam_role" "bastion" {
-  name               = "${local.prefix}-bastion"
-  assume_role_policy = file("./policies/bastion-ec2.json")
-
-  tags = local.common_tags
-}
-
 resource "aws_instance" "mxmd_bastion" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  user_data     = file("./user-data.sh")
+  ami                  = data.aws_ami.amazon_linux.id
+  instance_type        = "t2.micro"
+  user_data            = file("../scripts/user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  key_name             = var.bastion_key
+  # Swap the commented subnet config
+  #  lines when you need to visit resources
+  #  in a different subnet.
+  subnet_id = aws_subnet.public_a.id
+  # subnet_id = aws_subnet.public_b.id
   tags = merge(
     local.common_tags,
     map("Name", "${local.prefix}-bastion")
   )
+}
+
+resource "aws_iam_role" "bastion" {
+  name               = "${local.prefix}-bastion"
+  assume_role_policy = file("../policies/bastion-ec2.json")
+  tags               = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam:aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
 }
--- ci/tools/terraform/variables.tf
+++ ci/tools/terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "prefix" { default = "mxmd" }
 variable "project" { default = "newsfeed" }
 variable "contact" { default = "contato@maximinder.com.br" }
+variable "bastion_key" { default = "newsfeed-bastion" }
 variable "db_username" { description = "RDS database username" }
 variable "db_password" { description = "RDS database password" }
